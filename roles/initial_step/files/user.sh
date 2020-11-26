1f04eef891e572a6b0d47406961dc03066aa3842
Alessandro Bernardi <alexberny@yahoo.it>
2020-11-26 23:09:49 +0100

    added maintenance mode

--- roles/initial_step/files/user.sh
+++ roles/initial_step/files/user.sh
@@ -6,52 +6,57 @@ a=0
 showed_alert=0
 showed_loading=false
 
-##### set new hostname if hostname is set to default
-hostn=$(hostname)
-while [ hostname = "thin" ];
-do
-  if zenity --entry --title="Inserisci il nuovo hostname" --text="Hostname non settato, inserisci il nuovo hostname" 
-  then 
-    newhost= $? 
-    #change hostname in /etc/hosts & /etc/hostname
-    sudo sed -i "s/$hostn/$newhost/g" /etc/hosts
-    sudo sed -i "s/$hostn/$newhost/g" /etc/hostname
-    zenity --notification --window-icon="info" --text="Nuovo hostname inserito!" 
-    sudo reboot
-  fi
-done
+# if not exists .maintenance_mode file then execute
+if [ ! -f "~/.maintenance_mode"]
+then
 
-while :
-do
-  # get active interface
-  eths_dev=$(nmcli -t -f uuid,type,device c s --active | grep 802 | awk -F  ":" '{ print $3 }' | paste -s -d, -)
-  if [ -z "$eths_dev" ]
-  then
-    if [ ! "$showed_loading" ]
-    then
-      pqiv -t -i -f  /home/user/Pictures/REV.gif &
-      showed_loading = true
+  ##### set new hostname if hostname is set to default
+  hostn=$(hostname)
+  while [ hostname = "thin" ];
+  do
+    if zenity --entry --title="Inserisci il nuovo hostname" --text="Hostname non settato, inserisci il nuovo hostname" 
+    then 
+      newhost= $? 
+      #change hostname in /etc/hosts & /etc/hostname
+      sudo sed -i "s/$hostn/$newhost/g" /etc/hosts
+      sudo sed -i "s/$hostn/$newhost/g" /etc/hostname
+      zenity --notification --window-icon="info" --text="Nuovo hostname inserito!" 
+      sudo reboot
     fi
-    ((a+=1))
-    if [ $a -ge 4 ]
+  done
+
+  while :
+  do
+    # get active interface
+    eths_dev=$(nmcli -t -f uuid,type,device c s --active | grep 802 | awk -F  ":" '{ print $3 }' | paste -s -d, -)
+    if [ -z "$eths_dev" ]
     then
-      if [ "$showed_alert" = 0 ]
+      if [ ! "$showed_loading" ]
       then
-        showed_alert = 1
-        zenity --error --width=200 --text "Errore di rete, contattare l'ufficio ICT"
-        showed_alert = 0
+        pqiv -t -i -f  /home/user/Pictures/REV.gif &
+        showed_loading = true
       fi
-    fi
-  else
-    # reset condition
-    pkill -9 pqiv
-    showed_loading=false
-    a=0
-    # set current active interface for vino
-    eths_uuid=$(nmcli -t -f uuid,type c s --active | grep 802 | awk -F  ":" '{ print "'\''" $1 "'\''" }' | paste -s -d, -)
-    gsettings set org.gnome.settings-daemon.plugins.sharing.service:/org/gnome/settings-daemon/plugins/sharing/vino-server/ enabled-connections "[ $eths_uuid ]"
+      ((a+=1))
+      if [ $a -ge 4 ]
+      then
+        if [ "$showed_alert" = 0 ]
+        then
+          showed_alert = 1
+          zenity --error --width=200 --text "Errore di rete, contattare l'ufficio ICT"
+          showed_alert = 0
+        fi
+      fi
+    else
+      # reset condition
+      pkill -9 pqiv
+      showed_loading=false
+      a=0
+      # set current active interface for vino
+      eths_uuid=$(nmcli -t -f uuid,type c s --active | grep 802 | awk -F  ":" '{ print "'\''" $1 "'\''" }' | paste -s -d, -)
+      gsettings set org.gnome.settings-daemon.plugins.sharing.service:/org/gnome/settings-daemon/plugins/sharing/vino-server/ enabled-connections "[ $eths_uuid ]"
 
-    vmware-view --nomenubar
-  fi
-  sleep 15
-done
+      vmware-view --nomenubar
+    fi
+    sleep 15
+  done
+fi
