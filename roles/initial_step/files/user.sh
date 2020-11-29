8e0d8d933ccb2c8cdfcfb351e9355e8236affc11
Alessandro Bernardi <alexberny@yahoo.it>
2020-11-29 22:34:33 +0100

    replace sudo vith pkexec

--- roles/initial_step/files/user.sh
+++ roles/initial_step/files/user.sh
@@ -19,10 +19,9 @@ then
     do
       newhost=$(zenity --entry --title="Inserisci il nuovo hostname" --text="Hostname non settato, inserisci il nuovo hostname" --entry-text "$hostn")
     done
-    sudo sed -i "s/$hostn/$newhost/g" /etc/hosts
-    sudo sed -i "s/$hostn/$newhost/g" /etc/hostname
+    pkexec bash -c 'sed -i "s/$hostn/$newhost/g" /etc/hosts; sed -i "s/$hostn/$newhost/g" /etc/hostname'
     zenity --notification --window-icon="info" --text="Nuovo hostname inserito!" 
-    sudo reboot
+    pkexec reboot
   done
 
   while :
